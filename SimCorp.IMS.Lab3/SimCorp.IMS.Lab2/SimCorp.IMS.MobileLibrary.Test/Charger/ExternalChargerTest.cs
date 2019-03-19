19ae4626563a43412d2314b5c527a920855c005a
BRYA <BRYA@DK01WV6129>
2019-03-19 10:37:18 +0200

    Copy previous labs.

---
+++ SimCorp.IMS.Lab3/SimCorp.IMS.Lab2/SimCorp.IMS.MobileLibrary.Test/Charger/ExternalChargerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimCorp.IMS.Lab3;
+
+namespace MobileLibrary.Test {
+    [TestClass]
+    public class WirelessChargerTest {
+        [TestMethod]
+        public void ChargeValidOutputText() {
+            FakeOutput output = new FakeOutput();
+            WirelessCharger charger = new WirelessCharger(output,1.5f);
+
+            charger.Charge();
+
+            Assert.AreEqual("Charge with WirelessCharger\n", output.OutputResult);
+        }
+    }
+}
---
+++ SimCorp.IMS.Lab3/SimCorp.IMS.Lab2/SimCorp.IMS.WinFormsApp/WinLogOutput.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimCorp.IMS.Lab3;
+
+namespace WinFormsApp {
+    class WinLogOutput : IOutput {
+        private IWinAppLog WinForm { get; }
+
+        public WinLogOutput(IWinAppLog winForm) {
+            WinForm = winForm;
+        }
+
+        public void Write(string text) {
+            WinForm.LogText += text;
+        }
+
+        public void WriteLine(string text) {
+            WinForm.LogText += text + "\n";
+        }
+
+        public void Clean() {
+            WinForm.LogText = "";
+        }
+    }
+}
